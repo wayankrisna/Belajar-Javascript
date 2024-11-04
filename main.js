a873f0da5c9d08e6178f6d8a4e348973d0deafdc
wayankrisna <krisnayasa2018@gmail.com>
2024-11-04 11:35:56 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,74 @@
+// alert('hey buddy')
+// prompt('masukkan usia anda : ')
+// var usia = 19
+// const x = 1 
+// let y = 2
+// let usia = 20
+// usia = 35
+// console.log('heyhoo usia kamu adalah ' + usia)
+// alert ('usia kamu adalah ' + usia)
+// let usia  = prompt ('berapa usia kamu ? ')
+// alert('usia anda adalah ' + usia)
+// let nama = 'Krisna Yasa' //tipe data string
+// let usia = 19 //tipe data number
+// let tinggiBadan = 170.5 //tipe data double or float
+// let beratBadan
+// let pacar = 5
+// beratBadan = 68
+
+// if (pacar == null) {
+//     pacar = 'belum punya'
+// }
+// else {
+//     pacar = 'udah punya'
+// }
+
+// switch (pacar) {
+//     case 1:
+//         pacar = "punya satu aja"
+//     break
+//     case 2:
+//         pacar = "punya dua, saya cukup playboy"
+//     break
+//     default:
+//         pacar = "belum punya pacar"
+//     break
+// }
+
+// let saldoAwal = 50000
+// let saldoTambahan = 100000
+// const hutang = 30000
+// const saldoAkhir = saldoAwal + saldoTambahan - hutang
+// alert (`nama saya ${nama} usia saya itu ${usia} tinggi badan saya ${tinggiBadan} cm berat badan saya ${beratBadan} kg dan pacar saya ${pacar}`)
+// alert(`saldo awal saya sebesar Rp.${saldoAwal} & saldo tambahan saya sebesar Rp.${saldoTambahan} jadi total saldo saya sekarang ${saldoAkhir}`)
+
+//ARRAY
+// let namaGuru = ['Jodi', 'Bunga', 'Rahman']
+// namaGuru.push('Dea', 'Fikri')
+// namaGuru.shift()
+// namaGuru.pop()
+// alert (namaGuru)
+
+// let namaGuru =[]
+
+// namaGuru[0]= 'Dea'
+// namaGuru[1]= 'Fikri'
+// alert(namaGuru)
+
+//for loop
+// const namaGuru = ['Dea','Fikri','Retno','Bunga']
+// for (let i = 0; i < namaGuru.length; i++){
+//     console.log(namaGuru[i])
+// }
+
+//while loop
+// let i = 0
+// while (i < 10) {
+//     i++
+//     console.log('Krisna Yasa')
+// }
+
+// do {
+// i++
+// console.log('Krisna Yasa')
+// } while (i < 10)
